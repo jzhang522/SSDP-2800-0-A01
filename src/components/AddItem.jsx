509e8ea4fac8537b836b0963cde6bbfce3cbdeed
Jimmy <jzhang522@my.bcit.ca>
2025-11-24 00:09:43 -0800

    Adds form handler for  user input

--- src/components/AddItem.jsx
+++ src/components/AddItem.jsx
@@ -1,55 +1,78 @@
 import { useState } from "react";
-import Cart from "./Cart"
+import Cart from "./Cart";
 
 function AddItem() {
-    const [itemName, setItemName] = useState();
-    const [price, setPrice] = useState();
-    const [quantity, setQuantity] = useState();
+    const [itemName, setItemName] = useState("");
+    const [price, setPrice] = useState(0);
+    const [quantity, setQuantity] = useState(0);
+    const [itemArray, setItemArray] = useState([]);
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(itemName, price, quantity);
+
+        setItemName("");
+        setPrice(0);
+        setQuantity(0);
+
+    }
 
     return (
         <>
             <div className="add-item-container">
-                <h2 className="containter-header">Add Item</h2>
-                <label htmlFor="item-name" className="add-item-label"></label>
-                <input
-                    type="text"
-                    id="item-name"
-                    name="item-name"
-                    className="form-input-box"
-                    placeholder="Enter item name"
-                    value={itemName}
-                    onChange={(e) => setItemName(e.target.value)}
-                />
-                <label htmlFor="price" className="add-item-label"></label>
-                <input
-                    type="number"
-                    id="price"
-                    name="price"
-                    min="0"
-                    className="form-input-box"
-                    placeholder="Enter price"
-                    value={price}
-                    onChange={(e) => setPrice(e.target.value)}
-                />
-                <label htmlFor="quantity" className="add-item-label"></label>
-                <input
-                    type="number"
-                    id="quantity"
-                    name="quantity"
-                    min="0"
-                    max="100"
-                    step="1"
-                    className="form-input-box"
-                    placeholder="Enter quantity"
-                    value={quantity}
-                    onChange={(e) => setQuantity(e.target.value)}
-                />
-                <button type="submit" className="submit-button">
-                    Add to Cart
-                </button>
+                <form onSubmit={handleSubmit} className="add-item-form">
+                    <h2 className="containter-header">Add Item</h2>
+                    <label
+                        htmlFor="item-name"
+                        className="add-item-label"
+                    >Item Name:</label>
+                    <input
+                        type="text"
+                        id="item-name"
+                        name="item-name"
+                        className="form-input-box"
+                        placeholder="Enter item name"
+                        value={itemName}
+                        onChange={(e) => setItemName(e.target.value)}
+                    />
+                    <label htmlFor="price" className="add-item-label">Item Price:</label>
+                    <input
+                        type="number"
+                        id="price"
+                        name="price"
+                        min="0"
+                        className="form-input-box"
+                        placeholder="Enter price"
+                        value={price}
+                        onChange={(e) => setPrice(e.target.value)}
+                    />
+                    <label
+                        htmlFor="quantity"
+                        className="add-item-label"
+                    >Quantity</label>
+                    <input
+                        type="number"
+                        id="quantity"
+                        name="quantity"
+                        min="0"
+                        max="100"
+                        step="1"
+                        className="form-input-box"
+                        placeholder="Enter quantity"
+                        value={quantity}
+                        onChange={(e) => setQuantity(e.target.value)}
+                    />
+                    <button
+                        type="submit"
+                        className="submit-button"
+                        onSubmit={handleSubmit}
+                    >
+                        Add to Cart
+                    </button>
+                </form>
             </div>
 
-            <Cart/>
+            <Cart />
         </>
     );
 }
